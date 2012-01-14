c49ce3d6eb1a37b812fdde072c161a5779e6d606
Scott Robinson <scott.robinson@thoughtworks.com>
2012-01-14 20:20:16 +1100

    BLOW THEM UP

--- bad.lua
+++ bad.lua
@@ -9,19 +9,33 @@ function Bad:new()
 end
 
 function Bad:update(dt)
+    if self.state == "dying" then
+        self.death_duration = self.death_duration - dt
+
+        if self.death_duration < 0 then
+            self.state = "dead"
+        end
+    end
 end
 
 function Bad:hit(bullet)
-    table.insert(debug, "hit!")
+    self.state = "dying"
+    self.death_duration = 1
 end
 
 function Bad:draw()
     local size = 20
 
-    love.graphics.triangle(
-        'line',
-        self.x, self.y + (size / 2),
-        self.x + (size / 2), self.y - (size / 2),
-        self.x - (size / 2), self.y - (size / 2)
-    )
+    if self.state == "enter" then
+        love.graphics.triangle(
+            'line',
+            self.x, self.y + (size / 2),
+            self.x + (size / 2), self.y - (size / 2),
+            self.x - (size / 2), self.y - (size / 2)
+        )
+    elseif self.state == "dying" then 
+        local current_color = love.graphics.getColor()
+        love.graphics.setColor(math.random() * 255, math.random() * 255, math.random() * 255, self.death_duration * 255)
+        love.graphics.circle('fill', self.x, self.y, 50 * self.death_duration)
+    end
 end
