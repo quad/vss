722b0e979c5d17f3092028bf8f86bc015720478c
Scott Robinson <scott.robinson@thoughtworks.com>
2012-01-14 18:17:45 +1100

    Controller action on our ship. Also, we can quit.

--- main.lua
+++ main.lua
@@ -1,20 +1,32 @@
 x, y = 400, 300
 
-function love.update(dt)
-    if love.keyboard.isDown("left") then
-	x = x - 100 * dt
-    end
-    if love.keyboard.isDown("right") then
-	x = x + 100 * dt
-    end
-    if love.keyboard.isDown("up") then
-	y = y - 100 * dt
-    end
-    if love.keyboard.isDown("down") then
-	y = y + 100 * dt
-    end
+joystick = 0
+joystick_axis_lr = 0
+joystick_axis_ud = 1
+joystick_sensitivity = 500
+
+debug = {'lulz'}
+
+function love.load(arg)
+    love.joystick.open(joystick)
 end
 
+function love.update(dt)
+    -- left and 
+    debug = {
+        love.joystick.getAxis(joystick, 0),
+        love.joystick.getAxis(joystick, 1)
+    }
+
+    x = x + (joystick_sensitivity * love.joystick.getAxis(joystick, joystick_axis_lr)) * dt
+    y = y + (joystick_sensitivity * love.joystick.getAxis(joystick, joystick_axis_ud)) * dt
+end
+
+function love.keypressed(k)
+    if k == 'escape' then
+        love.event.push('q') -- quit the game
+    end 
+end
 
 function draw_ship(x, y)
     local size = 20
@@ -25,4 +37,9 @@ end
 
 function love.draw()
     draw_ship(x, y)
+
+    -- Draw the loaded lines.
+    for i = 1, #debug do
+        love.graphics.print("Line " .. i .. ": " .. debug[i], 50, 50 + (i * 10))
+    end
 end
